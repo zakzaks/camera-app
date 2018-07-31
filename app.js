7c8f1d184461989e1e25fb61f257d8997482c3a8
zakzaks <ariefn82@gmail.com>
2018-07-31 11:43:21 +0700

    trytofixbuttondelete(8)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function previewFile() {
-  var preview = document.querySelector('img');
-  var file    = document.querySelector('input[type=file]').files[0];
+  var preview = document.querySelector('#img');
+  var file    = document.querySelector('#fileinput').files[0];
   var reader  = new FileReader();
 
   reader.onloadend = function () {
@@ -14,8 +14,29 @@ function previewFile() {
   }
 }
 
-$('#clear').click(function (e) { 
-  e.preventDefault();
-  $('#fileinput').val("");
-});
+function previewFile2() {
+  var preview = document.querySelector('#img2');
+  var file    = document.querySelector('#fileinput2').files[0];
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "";
+  }
+}
+
+document.getElementById("clear").addEventListener("click", function () {
+  document.getElementById("fileinput").value = "";
+  previewFile();
+}, false);
+
+document.getElementById("clear2").addEventListener("click", function () {
+  document.getElementById("fileinput2").value = "";
+  previewFile2();
+}, false);
 
